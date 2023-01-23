ce45cd8f08854dca6ff66718f1f80774fe70d68b
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-01-22 21:58:26 -0800

    Behind the scenes images imported

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -5,7 +5,7 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 import { Link } from 'react-router-dom'
 const About = () => {
     const { scrollYProgress } = useScroll();
-    const x = useTransform(scrollYProgress, [0, 1], [1000, -3000], [0.23, -0.9, 0.34, 0.99] )
+    const x = useTransform(scrollYProgress, [0, 1], [1000, -4000], [0.23, -0.9, 0.34, 0.99] )
 
 
     const [mousePosition, setMousePosition] = useState({
@@ -70,7 +70,8 @@ const About = () => {
         transition={{duration: 1.4, delay: 0.3}}
         viewport={{once: true}}
         className='about-text'>
-            <p className='about-p'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim commodi, amet alias nisi nostrum minima atque perspiciatis molestiae libero, doloribus nesciunt. Consequuntur nemo ad adipisci expedita! Maiores vitae labore blanditiis magni ut quibusdam error assumenda? Dignissimos mollitia nulla voluptas rerum possimus! Atque quo unde iste.</p>
+            <p className='about-p'> 
+            My name is Kirubel Fikremariam and I'm a CG artist based in Addis Abeba, Ethiopia. I was first introduced to the world of 3D graphics in 2019 and have been obsessed with it ever since. I've had an interest in art specifically sketching prior to my knowledge of CG which was influential in my learning journey. I'm skilled in a number of softwares namely Blender, Substance painter, and Photoshop. I've participated in International art contests such as the annual "Rookies Awards" in 2022 and ended up as a finalist. I was also in the top 100 3D Renders from the Internet's Biggest CG Challenge, "Infinite Journeys".</p>
             </motion.div>
         <motion.div 
         
@@ -88,7 +89,7 @@ const About = () => {
         whileInView={{opacity: 1}}
         transition={{duration: 1.2, delay: 0.8}}
         viewport={{once: true}}
-        href="#" className='contact-cta'  onMouseEnter={textEnter} onMouseLeave={textLeave}>
+        href="mailto:kirubfm@gmail.com" target='_blank' className='contact-cta'  onMouseEnter={textEnter} onMouseLeave={textLeave}>
                 <p>CONTACT ME</p>
             </motion.a>
         </div>
